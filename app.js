7b5d99aeba2aba7250a2e84eb55466391c0bbce9
Danny <domezero@gmail.com>
2022-12-14 23:29:56 -0600

    latest

--- app.js
+++ app.js
@@ -2,111 +2,95 @@ const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const helmet = require('helmet');
-const ejs = require('ejs');
-const { performance } = require('perf_hooks');
-const computeOverlappedVars = require('./census-data');
+
+const getCensusData = require('./apis/census-data');
+const getPropData = require('./apis/homepage-property-data');
+const buildDashPage = require('./apis/queryPGforDashData');
+const registerController = require('./controllers/registerController');
+const authController = require('./controllers/authController');
+const refreshTokenController = require('./controllers/refreshTokenController');
+const logoutController = require('./controllers/logoutController');
+
+// const verifyJWT = require('./middleware/verifyJWT');
 const bodyParser = require('body-parser');
-const passport = require('passport')
-const { Strategy } = require('passport-google-oauth20')
-const cookieSession = require('cookie-session');
+
+const cookieParser = require('cookie-parser');
 const { verify } = require('crypto');
+const cors = require('cors');
+const cloudinary = require('cloudinary');
 
+// var whitelist = ['http://localhost:3000']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
+cloudinary.config({
+  cloud_name: 'djlalgsmk',
+  api_key: '386655761436352',
+  api_secret: 'Ous8jCUrug-uZH7OVLRfkZCKARk'
+});
 
 require('dotenv').config();
 
-const config = {
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  COOKIE_KEY_A: process.env.COOKIE_KEY_A,
-  COOKIE_KEY_B: process.env.COOKIE_KEY_B,
-};
 
-const GOOGLE_AUTH_OPTIONS = {
-  callbackURL: '/auth/google/callback',
-  clientID: config.GOOGLE_CLIENT_ID,
-  clientSecret: config.GOOGLE_CLIENT_SECRET,
-};
-
-passport.use(new Strategy(GOOGLE_AUTH_OPTIONS, verifyGoogleCallback));
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-}); //Save the session to the cookie
-
-passport.deserializeUser((id, done) => {
-  done(null, id);
-}); //Read the session from the cookie
 
 const app = express();
+app.options('*', cors());
+app.use(cors());
 app.use(helmet());
-app.use(cookieSession({
-  name: 'session',
-  maxAge: 24 * 60 * 60 * 1000,
-  keys: [config.COOKIE_KEY_A, config.COOKIE_KEY_B],
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.set('view engine', 'ejs');
+app.use(cookieParser());
+// app.use(cookieSession({
+//   name: 'session',
+//   maxAge: 24 * 60 * 60 * 1000,
+//   keys: [config.COOKIE_KEY_A, config.COOKIE_KEY_B],
+// }));
+app.use(express.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', (req, res) => {
-  res.render('login');
+//Routes
+
+//Recieve user registration data from react
+app.post('/register', registerController.handleNewUser);
+
+app.post('/signin', authController.handleLogin);
+
+app.get('/refresh', refreshTokenController.handleRefreshToken);
+
+app.get('/logout', logoutController.handleLogout);
+
+//sends dashboard data for specific dash id to react
+app.get('/data/:id', (req, res) => {
+  const queryParam = parseInt(req.params.id)
+  buildDashPage.query(queryParam).then((result) => {
+    res.json(result);
+  })
 });
 
-app.get('/app', (req, res) => {
-  res.render('home');
+//sends homepage data to react
+app.get('/properties', (req, res) => {
+  getPropData.getPropertyData().then((result) => {
+    res.json(result);
+  });
 });
 
-app.get('/dashboard', (req, res) => {
-  res.render('dashboard')
-})
-
-
-app.post('/app', (req, res) => {
-  //Save the user input(address)
-  const propAddress = req.body.property;
-
-  computeOverlappedVars.computeOverlappedVars(propAddress).then(function (result){
-    const headers = Object.keys(result)
-    const variables = Object.values(result)
-    res.render('results', {data: variables, names: headers});
-  }); //works
-
-
+//Recieves user property form data and compiles results to pg
+app.post('/posts', (req, res) => {
+  const propAddress = req.body.address;
+  getCensusData.calculateData(propAddress).then(function(result) {
+    res.send(result); //Gets id of postgres insert
+  });
 });
 
 
-
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['email'],
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google', {
-  failureRedirect: '/failure',
-  successRedirect: '/',
-  session: true,
-}), (req, res) => {
-  console.log('Google called us back!');
-});
-
-app.get('/auth/logout', (req, res) => {
-  req.logout(); //Removes req.user and clears any logged in session
-  res.redirect('/');
-});
-
-app.get('/secret', checkLoggedIn, (req, res) => {
-  return res.send('Your secret is gay...');
-});
-
-app.get('/failure', (req, res) => {
-  return res.send('Failed to login!');
-})
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 https.createServer({
   key: fs.readFileSync('key.pem'),
@@ -121,18 +105,66 @@ https.createServer({
 
 
 //Check to see if user is logged in
-function checkLoggedIn(req, res, next) {
-  // console.log('Current user is:', req.user);
-  const isLoggedIn = req.isAuthenticated() && req.user;
-  if (!isLoggedIn) {
-    return res.status(401).json({
-      error: 'You must log in!',
-    });
-  }
-  next();
-}
+// function checkLoggedIn(req, res, next) {
+//   // console.log('Current user is:', req.user);
+//   const isLoggedIn = req.isAuthenticated() && req.user;
+//   if (!isLoggedIn) {
+//     return res.status(401).json({
+//       error: 'You must log in!',
+//     });
+//   }
+//   next();
+// }
+//
+// function verifyGoogleCallback(accessToken, refreshToken, profile, done) {
+//   console.log('Google profile', profile);
+//   done(null, profile);
+// };
 
-function verifyGoogleCallback(accessToken, refreshToken, profile, done) {
-  console.log('Google profile', profile);
-  done(null, profile);
-};
+// app.get('/auth/google', passport.authenticate('google', {
+//   scope: ['email'],
+// }));
+//
+// app.get('/auth/google/callback', passport.authenticate('google', {
+//   failureRedirect: '/failure',
+//   successRedirect: '/',
+//   session: true,
+// }), (req, res) => {
+//   console.log('Google called us back!');
+// });
+//
+// app.get('/auth/logout', (req, res) => {
+//   req.logout(); //Removes req.user and clears any logged in session
+//   res.redirect('/');
+// });
+//
+// app.get('/secret', checkLoggedIn, (req, res) => {
+//   return res.send('Your secret is gay...');
+// });
+//
+// app.get('/failure', (req, res) => {
+//   return res.send('Failed to login!');
+// })
+
+// const config = {
+//   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+//   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+//   COOKIE_KEY_A: process.env.COOKIE_KEY_A,
+//   COOKIE_KEY_B: process.env.COOKIE_KEY_B,
+// };
+//
+// const GOOGLE_AUTH_OPTIONS = {
+//   callbackURL: '/auth/google/callback',
+//   clientID: config.GOOGLE_CLIENT_ID,
+//   clientSecret: config.GOOGLE_CLIENT_SECRET,
+// };
+//
+// passport.use(new Strategy(GOOGLE_AUTH_OPTIONS, verifyGoogleCallback));
+//
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// }); //Save the session to the cookie
+//
+// passport.deserializeUser((id, done) => {
+//   done(null, id);
+// }); //Read the session from the cookie
